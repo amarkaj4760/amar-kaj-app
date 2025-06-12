6e718c51c65a20886878801b3af2a18a474b43f7
Amar kaj <amarkaj4760@gmail.com>
2025-06-12 20:55:57 +0600

    Update main.dart to launch LoginScreen

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,22 @@
 import 'package:flutter/material.dart';
-import 'splash_screen.dart';
+import 'screens/login_screen.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(const AmarKajApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class AmarKajApp extends StatelessWidget {
+  const AmarKajApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Amar Kaj',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const LoginScreen(),
       debugShowCheckedModeBanner: false,
-      home: const SplashScreen(),
     );
   }
 }
