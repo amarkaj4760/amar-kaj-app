1b3045934e36656ddb8b94e4e346d6af2e0c661f
Amar kaj <amarkaj4760@gmail.com>
2025-06-12 19:43:22 +0600

    Update main.dart to use splash screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,53 +1,19 @@
 import 'package:flutter/material.dart';
+import 'splash_screen.dart';
 
 void main() {
-  runApp(AmarKajApp());
+  runApp(const MyApp());
 }
 
-class AmarKajApp extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Amar Kaj',
       debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primarySwatch: Colors.blue,
-      ),
-      home: SplashScreen(),
-    );
-  }
-}
-
-class SplashScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.white,
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Icon(Icons.work, size: 100, color: Colors.blue),
-            SizedBox(height: 20),
-            Text(
-              'Amar Kaj',
-              style: TextStyle(fontSize: 28, fontWeight: FontWeight.bold),
-            ),
-            SizedBox(height: 40),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('I Need Work'),
-              style: ElevatedButton.styleFrom(minimumSize: Size(200, 50)),
-            ),
-            SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: () {},
-              child: Text('I Need Worker'),
-              style: ElevatedButton.styleFrom(minimumSize: Size(200, 50)),
-            )
-          ],
-        ),
-      ),
+      home: const SplashScreen(),
     );
   }
 }
